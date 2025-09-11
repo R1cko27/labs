9e51d853b303d49b4ea58785761594e0356beaaa
R1cko27 <panfilovartem128@gmail.com>
2025-09-11 17:48:41 +0700

    1

---
+++ course_1/Лабораторная_1/Задание_2/ЛР1_АП526_2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+
+int Count_Pos(int number) {
+    int p = 1;
+    while ((number / (int)pow(10, p)) > 0) p++;
+    return p;
+}
+
+void answer(int number) {
+    int countOfNumber = 0;
+    int i = 1;
+    while (countOfNumber < number) {
+        printf("%d", i);
+        countOfNumber += Count_Pos(i);
+        i++;
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "Rus");
+    int numberInput;
+    printf("Введите число ");
+    scanf_s("%d", &numberInput);
+    answer(numberInput);
+}
