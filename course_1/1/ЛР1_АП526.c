a298ae249eb91bdaa83a70a14c4ee03b46e70f09
R1cko27 <panfilovartem128@gmail.com>
2025-09-11 17:10:31 +0700

    1

--- course_1/1/ЛР1_АП526.c
+++ course_1/1/ЛР1_АП526.c
@@ -1,36 +1,36 @@
 #include <locale.h>
 #include <stdio.h>
 
-void type1(int number){
+void type1(int number) {
     int maxNumber;
-    for (int i = 0; i < number + 1; i++){
+    for (int i = 0; i < number; i++) {
         int temp_num;
-        printf("Введите %d число для типа 1 ", i+1);
+        printf("Введите %d число для типа 1 ", i + 1);
         scanf_s("%d", &temp_num);
-        if (i == 0){
+        if (i == 0) {
             maxNumber = temp_num;
-        } 
+        }
         else {
-            if (temp_num > maxNumber){
+            if (temp_num > maxNumber) {
                 maxNumber = temp_num;
             }
         }
-    printf("Максимальное число в типе 1: %d\n", maxNumber);
+        printf("Максимальное число в типе 1: %d\n", maxNumber);
     }
 }
 
-void type2(int number){
+void type2(int number) {
     int maxNumber;
     int i = 0;
-    while (i < number){
+    while (i < number) {
         int temp_num;
-        printf("Введите %d число для типа 2 ", i+1);
+        printf("Введите %d число для типа 2 ", i + 1);
         scanf_s("%d", &temp_num);
-        if (i == 0){
+        if (i == 0) {
             maxNumber = temp_num;
         }
         else {
-            if (temp_num > maxNumber){
+            if (temp_num > maxNumber) {
                 maxNumber = temp_num;
             }
         }
@@ -39,18 +39,18 @@ void type2(int number){
     printf("Максимальное число в типе 2: %d\n", maxNumber);
 }
 
-void type3(int number){
+void type3(int number) {
     int maxNumber;
     int i = 0;
     do {
         int temp_num;
-        printf("Введите %d число для типа 3 ", i+1);
+        printf("Введите %d число для типа 3 ", i + 1);
         scanf_s("%d", &temp_num);
-        if (i == 0){
+        if (i == 0) {
             maxNumber = temp_num;
         }
-        else{
-            if (temp_num > maxNumber){
+        else {
+            if (temp_num > maxNumber) {
                 maxNumber = temp_num;
             }
         }
@@ -59,7 +59,7 @@ void type3(int number){
     printf("Максимальное число в типе 3: %d\n", maxNumber);
 }
 
-int main(){
+int main() {
     int number;
     int chose;
     setlocale(LC_ALL, "Rus");
@@ -67,13 +67,16 @@ int main(){
     scanf_s("%d", &number);
     printf("Выбирите функцию, которую хотите использовать\n1. Цикл for (type1)\n2. Цикл while (type2)\n3. Цикл do (type3)\n");
     scanf_s("%d", &chose);
-    if (chose == 1){
+    if (chose == 1) {
         type1(number);
-    } else if (chose == 2){
+    }
+    else if (chose == 2) {
         type2(number);
-    } else if (chose == 3){
+    }
+    else if (chose == 3) {
         type3(number);
-    } else {
+    }
+    else {
         printf("Неизвестный выбор");
     }
 }
