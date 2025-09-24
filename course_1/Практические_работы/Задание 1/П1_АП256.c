cae6b0883fafda4089ba70767aaca609fedfd404
R1cko27 <panfilovartem128@gmail.com>
2025-09-24 12:47:16 +0700

    1

--- course_1/Практические_работы/Задание 1/П1_АП256.c
+++ course_1/Практические_работы/Задание 1/П1_АП256.c
@@ -4,12 +4,15 @@ void answer(int a, int b, int c){
     int new_c = c < a && c < b ? c : a < c && a < b ? a : b;
     int new_a = a > c && a > b ? a : c > a && c > b ? c : b;
     int new_b = a + b + c - new_c - new_a;
-    printf("a = %d b = %d c = %d", new_a, new_b, new_c);
+    printf("Answer: a = %d b = %d c = %d", new_a, new_b, new_c);
 }
 int main(){
     int a,b,c;
+    printf("Enter a ");
     scanf_s("%d", &a);
+    printf("Enter b ");
     scanf_s("%d", &b);
+    printf("Enter c ");
     scanf_s("%d", &c);
     answer(a, b, c);
     return 0;
