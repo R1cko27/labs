f8858fc3853ea84dfa87724bfcd50a3d6bb4726f
R1cko27 <panfilovartem128@gmail.com>
2025-10-09 16:32:21 +0700

    1

--- course_1/Лабораторная_2/Задание_1/ЛР2_АП526_1.c
+++ course_1/Лабораторная_2/Задание_1/ЛР2_АП526_1.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
+#include <locale.h>
 #include <ctype.h>
 #include <string.h>
 
-void func(char *string){
+void func(char* string) {
     char new_string[1000] = "";
     char char_delet;
     int j = 0;
     string[strcspn(string, "\n")] = '\0';
-    
+    printf("Ответ: ");
     for (int i = 0; i < strlen(string); i++) {
         if (i == 0) {
             char_delet = string[i];
             printf("%c", string[i]);
             new_string[j++] = string[i];
         }
-        
+
         if (char_delet == '0') {
             if (isalpha(string[i])) {
                 char_delet = string[i];
@@ -22,23 +23,23 @@ void func(char *string){
                 new_string[j++] = string[i];
             }
         }
-        
+
         if (string[i] == char_delet) {
             printf("");
-        } else {
+        }
+        else {
             printf("%c", string[i]);
             new_string[j++] = string[i];
         }
-        
+
         if (!isalpha(string[i])) {
             char_delet = '0';
         }
     }
-    
-    new_string[j] = '\0';
-    printf("\nРезультирующая строка: %s\n", new_string);
+
 }
 int main() {
+    setlocale(LC_ALL, "Rus");
     char string[1000];
     printf("Введите строку: ");
     fgets(string, sizeof(string), stdin);
