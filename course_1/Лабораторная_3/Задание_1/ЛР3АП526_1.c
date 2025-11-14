e0589df02e4e61563722648b3aa03fa059f732ae
R1cko27 <panfilov@artem.email>
2025-11-14 10:39:02 +0700

    Update ЛР3АП526_1.c
    Update

--- course_1/Лабораторная_3/Задание_1/ЛР3АП526_1.c
+++ course_1/Лабораторная_3/Задание_1/ЛР3АП526_1.c
@@ -3,71 +3,73 @@
 #include <string.h>
 #include <stdlib.h>
 #include <windows.h>
+#include <locale.h>
 
 #define MAXLEN 150 // Максимальная длина строки
 #define L 260 // Максимальная длина файла
 
-char* Del_Str(char* str){
-    if (str == NULL) return str;
+char* Del_Str(char* str) {
+   if (str == NULL) return str;
 
-    // Проверяем, есть ли в строке хотя бы одна цифра
-    int has_digit = 0;
-    char *temp = str;
-    while (*temp != '\0') {
-        if (*temp >= '0' && *temp <= '9') {
-            has_digit = 1;
-            break;
-        }
-        temp++;
-    }
-    
-    // Если цифр нет - возвращаем пустую строку
-    if (!has_digit) {
-        str[0] = '\0';
-        return str;
-    }
-    
-    return str;
+   // Проверяем, есть ли в строке хотя бы одна цифра
+   int has_digit = 0;
+   char* temp = str;
+   while (*temp != '\0') {
+       if (*temp >= '0' && *temp <= '9') {
+           has_digit = 1;
+           break;
+       }
+       temp++;
+   }
+
+   // Если цифр нет - возвращаем пустую строку
+   if (has_digit == 0) {
+       str[0] = '\0';
+       return str;
+   }
+
+   return str;
 }
 
-void answer(FILE *f1, FILE *f2) {
-    char str[MAXLEN], *tmp;
-    
-    while(fgets(str, sizeof(str), f1) != NULL)
-    {
-        tmp = Del_Str(str);
-        fputs(tmp, f2);
-    }
-    
-    fclose(f1);
-    fclose(f2);
-    
-    puts("The operation is complete. The result is written to the result1.txt file");
+void answer(FILE* f1, FILE* f2) {
+   char str[MAXLEN], * tmp;
+
+   while (fgets(str, sizeof(str), f1) != NULL)
+   {
+       tmp = Del_Str(str);
+       fputs(tmp, f2);
+   }
+
+   fclose(f1);
+   fclose(f2);
+
+   puts("Операция завершена. Результат записан в файл result1.txt.");
 }
 
 int main()
 {
-    FILE *f1, *f2;
-    char fname[L], s[L];
+   setlocale(LC_ALL, "Rus");
+   FILE* f1, * f2;
+   char fname[L];
 
-    puts("\n Excluding a substring from the lines of a file.\n Enter the file name:");
-    fgets(fname, sizeof(fname), stdin);
-    fname[strcspn(fname, "\n")] = 0;
-    
-    if ((f1 = fopen(fname, "r")) == NULL)
-    { 
-        puts("Error when opening the source file!"); 
-        return 1;
-    }
-    
-    if ((f2 = fopen("result1.txt", "w")) == NULL)
-    {
-        puts("An error occurred while creating the result file!"); 
-        fclose(f1);
-        return 1;
-    }
-    
-    answer(f1, f2);
-    
-    return 0;
+   puts("Введите имя файла:\n");
+   fgets(fname, sizeof(fname), stdin);
+   fname[strcspn(fname, "\n")] = 0;
+
+   if ((f1 = fopen(fname, "r")) == NULL)
+   {
+       puts("Ошибка при открытии исходного файла!");
+       return 1;
+   }
+
+   if ((f2 = fopen("result1.txt", "w")) == NULL)
+   {
+       puts("Произошла ошибка при создании файла результата!");
+       fclose(f1);
+       return 1;
+   }
+
+   answer(f1, f2);
+
+   return 0;
 }
